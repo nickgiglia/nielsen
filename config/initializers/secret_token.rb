c3f2a36a7f79281e452d91d04166e2c862271800
U-NickGiglia\Nick <Nick@NickGiglia.(none)>
2011-01-13 11:09:03 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nielsen::Application.config.secret_token = '3a4627c8db3602df57b6855a8b7ca535f96fea02c069a10e90914afc26ba759b8e74a1353bdd8e5b41160c63d15660a6e61a44ba0d261d9bdc5287e30bc13b02'
